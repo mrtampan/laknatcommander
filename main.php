6b21c949bac04c38c9ca140367b778391065631e
achmadrivaldi <mrtampan54@gmail.com>
2022-09-02 05:11:22 +0700

    conditional

--- main.php
+++ main.php
@@ -2,10 +2,20 @@
 
 $file = readline('Masukan nama file command nya:');
 
+if(empty($file)){
+    echo "harap isi nama file terlebih dahulu";
+    exit();
+}
+
+if(!file_exists($file)){
+    echo "File tidak ditemukan";
+    exit();
+}
+
+
 $theFile = file_get_contents($file);
 
-$allCommand = preg_split("/\r\n|\n|\r/", $theFile);
-
+$allCommand = explode("\n", $theFile);
 
 foreach($allCommand as $command){
     echo shell_exec($command);
